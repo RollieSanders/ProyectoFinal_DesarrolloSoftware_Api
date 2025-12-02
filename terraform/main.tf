8374414574a3334a3a7139a1cb78e5eafdca4639
Rollie Ruiz <ruizolortegui87@gmail.com>
2025-12-01 23:50:45 -0500

    Terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,27 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+# Route table para subredes públicas (necesaria para acceso a Internet desde subnets públicas)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+# Asociaciones de route table a las subredes públicas
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 # Subredes Públicas (donde irá el ALB)
 resource "aws_subnet" "public" {
   count             = length(var.public_subnet_cidrs)
